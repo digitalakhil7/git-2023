055b4209d6fd269b682e8de708db835e1fb74404
digitalakhil7 <63640168+digitalakhil7@users.noreply.github.com>
2023-01-26 11:08:14 +0530

    Update App.java

--- src/main/java/com/akhil/App.java
+++ src/main/java/com/akhil/App.java
@@ -10,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        int a = 10;
     	Student akhil = new Student(101,"Akhil");
     	System.out.println("Student Details: "+akhil.getSid()+" "+akhil.getSname());
     }
