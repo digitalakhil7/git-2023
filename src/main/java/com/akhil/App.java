2188cc6f5c6b0c38786f98de9607c71e0c2d2f43
akhilgit <akhil@test.com>
2023-01-26 15:05:24 +0530

    removed junk

--- src/main/java/com/akhil/App.java
+++ src/main/java/com/akhil/App.java
@@ -10,23 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        long l = 30;
     	Student akhil = new Student(101,"Akhil");
     	System.out.println("Student Details: "+akhil.getSid()+" "+akhil.getSname());
     }
-        public void m1() {
-    	System.out.println("method m1");
-    }
-        public void m2() {
-        	System.out.println("method m2");
-        }
-        public void test() {
-        	
-        }
-        public void test1() {
-        	
-        }
 }
